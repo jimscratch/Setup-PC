d59683a65d1ddf0fe41a907e029de99d319de947
Zezypisa <49347779+Zezypisa@users.noreply.github.com>
2021-08-06 17:30:01 -0400

    Update Setup PC.ps1

--- Setup PC.ps1
+++ Setup PC.ps1
@@ -1,4 +1,4 @@
-#Invoke-Expression (New-Object System.Net.WebClient).DownloadString(https://raw.githubusercontent.com/Zezypisa/Setup-PC/main/Setup%20PC.ps1)
+#Invoke-Expression (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/Zezypisa/Setup-PC/main/Setup%20PC.ps1")
 
 $host.UI.RawUI.WindowTitle = "Setup PC"
 #Reinstalls programs I use commonly use, and set registry information with a simple script
